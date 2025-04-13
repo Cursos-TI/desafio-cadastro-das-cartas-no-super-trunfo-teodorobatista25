8903ae6c7c9fe388ea73c3f24f209fb320d8163a
teodorobatista25 <teodoro.tomas@gmail.com>
2025-04-13 22:00:08 +0000

    Super_Trumfo2.c

---
+++ Super_Trumfo 2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+
+    char Estado;
+    char Carta[30];
+    char Cidade[50];
+    int Populacao, Turismo;
+    float Area, PIB;
+
+
+
+    printf("Digite seu Estado: \n");
+    scanf("%c", &Estado);
+
+    printf(" Digite sua Carta \n");
+    scanf("%s", &Carta);
+
+    printf(" Digite sua Cidade \n");
+    scanf("%s", &Cidade);
+    
+    printf("Digite sua População \n");
+    scanf("%i", &Populacao);
+
+    printf("Digite sua Area \n");
+    scanf("%f", &Area);
+
+    printf("Digite seu PIB \n ");
+    scanf("%f", &PIB);
+
+    printf("Digite seu Turismo \n");
+    scanf("%d", &Turismo);
+
+    printf("Letra do Estado: %c\n", Estado);
+    printf("Número da Carta: %s\n", Carta);
+    printf("Nome da Cidade: %s\n", Cidade);
+    printf("Número Populacional: %i\n", Populacao);
+    printf("Área em Km²: %.3f\n", Area);
+    printf("PIB: %.2f\n", PIB);
+    printf("Números de Pontos Turísticos: %d\n", Turismo);
+
+
+    return 0;
+
+
+}
+
+
